6ca6dc8784e90adb9a2cfb7db4c97b1729bbf0ee
Nathan <nslee333@gmail.com>
2022-11-02 04:53:22 -0700

    WIP: Added buttons, input and some styling.

--- web_calculator/my-app/src/App.tsx
+++ web_calculator/my-app/src/App.tsx
@@ -4,9 +4,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <button>Button</button>
-    </div>
+
+
+    <div className='App'>
+      <input className='input-div' placeholder='123'></input>
+
+        <div className="btn-div">
+          
+          <button className='btn'>Multiply</button>
+          <button className='btn'>Equals</button>
+          <button className='btn'>Divide</button>
+          <button className='btn'>Add</button>
+          <button className='btn'>Subtract</button>
+          <button className='btn'>0</button>
+          <button className='btn'>1</button>
+          <button className='btn'>2</button>
+          <button className='btn'>3</button>
+          <button className='btn'>4</button>
+          <button className='btn'>5</button>
+          <button className='btn'>6</button>
+          <button className='btn'>7</button>
+          <button className='btn'>8</button>
+          <button className='btn'>9</button>
+
+        </div>
+      </div>
   );
 }
 
