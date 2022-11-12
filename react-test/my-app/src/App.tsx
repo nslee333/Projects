72e21220b2d32066219f38ee4d05dd76032d4959
Nathan <nslee333@gmail.com>
2022-11-11 23:04:55 -0800

    [WIP] Figuring out a way to delete individual characters from a number.

--- react-test/my-app/src/App.tsx
+++ react-test/my-app/src/App.tsx
@@ -6,29 +6,65 @@ import {useState, useEffect} from 'react';
 function App() {
   const [inputArray, setInputArray] = useState<string[]>([]);
   const [numberA, setNumberA] = useState(0);
+  const [numberb, setNumberB] = useState(0);
+  const [action, setAction] = useState(false);
+  const [actionValue, setActionValue] = useState('');
 
-  function addOne() {
-    inputArray.push("1");
-    inputArray.push(".")
-    console.log(inputArray);
-    arrayToNumber();
-  }
-
-  function arrayToNumber() {
-    const value: string = inputArray.join("");
-    const valueNumber: number = parseFloat(value);
-    setNumberA(valueNumber);
-  }
 
   useEffect(() => {
-    console.log("UseEffect log");
-    arrayToNumber();
-  }, [addOne])
+    process("");
+  }, [action])
+
+  
+  function process(inputNumber: string) {
+    console.log(inputNumber);
+    if (action === true) {
+      const result: number = parseArray(inputArray);
+      setNumberA(result);
+      console.log(result);
+    } else {
+      inputArray.push(inputNumber);
+      console.log(inputNumber);
+    }
+  }
+
+  function parseArray(array: string[]) {
+    const arrayString: string = array.join("");
+    const finalResult: number = parseFloat(arrayString);
+    return finalResult;
+  }
+
+  function del() {
+    const floatString: string = `${numberA}`;
+    const splitArray: string[] = floatString.split("");
+    console.log(splitArray);
+
+    splitArray.pop();
+    if (splitArray.length === 0) {
+      splitArray.push("0");
+    }
+    const arrayString: string = splitArray.join("");
+    const result: number = parseFloat(arrayString);
+    
+    setNumberA(result);
+
+  }
+
+  function setActionChar(inputValue: string) {
+    setActionValue(inputValue);
+  }
+
+  function calculate() {
+
+  }
 
 
   function myComponent() {
     return (
-      {inputArray}
+      <div>
+        {numberA}
+        {inputArray}
+      </div>
     );
   };
 
@@ -38,11 +74,26 @@ function App() {
   return (
     <>
       <div className="App">
-      <button onClick={async () => (addOne())}>Click 1</button>
-      <button onClick={async () => (addOne())}>+</button>
+      <button className='btn' onClick={async () => (process("1"))}>1</button>
+      <button className='btn' onClick={async () => (process("2"))}>2</button>
+      <button className='btn' onClick={async () => (process("3"))}>3</button>
+      <button className='btn' onClick={async () => (process("4"))}>4</button>
+      <button className='btn' onClick={async () => (process("5"))}>5</button>
+      <button className='btn' onClick={async () => (process("6"))}>6</button>
+      <button className='btn' onClick={async () => (process("7"))}>7</button>
+      <button className='btn' onClick={async () => (process("8"))}>8</button>
+      <button className='btn' onClick={async () => (process("9"))}>9</button>
+      <button className='btn' onClick={async () => (process("0"))}>0</button>
+      <button className='btn' onClick={async () => (process("."))}>.</button>
+      <button className='btn' onClick={async () => (setActionChar("X"))}>x</button>
+      <button className='btn' onClick={async () => (setActionChar("/"))}>/</button>
+      <button className='btn' onClick={async () => (setActionChar("+"))}>+</button>
+      <button className='btn' onClick={async () => (setActionChar("-"))}>-</button>
+      <button className='btn' onClick={async () => (setAction(true))}>=</button>
+      <button className='btn' onClick={async () => (del())}>delete</button>
     </div>
       <div>
-        {numberA}
+        {myComponent()}
       </div>
     </>
   );
--- react-testing-folder/my-app/todo.md
+++ react-testing-folder/my-app/todo.md
@@ -3,6 +3,7 @@ React testing todo:
 
 1. Add decimal precise library calculations to calculation() function.
 2. Chaining together calculations
+3. Deleting one character at a time.
 
 Current task:
 - Chaining together calculations:
