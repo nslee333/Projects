de15b3aa77470fa5763ea21460d8fc969172af1b
Nathan <nslee333@gmail.com>
2022-11-19 15:46:56 -0800

    [WIP]: Building front-end.

--- chat_app/my-app/src/App.tsx
+++ chat_app/my-app/src/App.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
-
-
-function chat_window() {
-  return (
-    <div className='chat-window-parent'>
-      Hello World
-      <div className='chat-input-bar'>
-        <div className='chat-input-div'>
-          <input placeholder='send a message' className='chat-input'></input>
-        </div>
-        <button className='chat-btn'>Send</button>
-      </div>
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
 function App() {
+  const [messagesArray, setMessagesArray] = useState<string[]>([]);
+
+  const sendMessage = (event: any) => {
+    // const message: string = 'chat-input'.value;
+    console.log(event.target.value)
+    messagesArray.push(event.target.value);
+  }
+  
+  
+  
+  function chat_window() {
+    return (
+      <div className='chat-window-parent'>
+        Hello World
+        <div className='chat-input-bar'>
+          <div className='chat-input-div'>
+            <input type='text' placeholder='send a message' className='chat-input' form='chat' onChange={sendMessage}/>
+          </div>
+          <button className='chat-btn' form='chat' onClick={sendMessage}>Button</button>
+        </div>
+      </div>
+    );
+  }
+  
+
+
+
+
+
   return (
     <div className="App">
       <div>{chat_window()}</div>
