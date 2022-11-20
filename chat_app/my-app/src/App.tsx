45196c9edced8188ae087c6587070e25f7303c0d
Nathan <nslee333@gmail.com>
2022-11-20 13:32:58 -0800

    [WIP]: Building front-end for chat app

--- chat_app/my-app/src/App.tsx
+++ chat_app/my-app/src/App.tsx
@@ -2,48 +2,7 @@ import React, { useEffect } from 'react';
 import {useState} from 'react';
 import './App.css';
 
-function App() {
-  const [messagesArray, setMessagesArray] = useState<string[]>([]);
-  const [draft, setDraft] = useState<string>('');
-  const [theme, setTheme] = useState(false);
-  
-  useEffect(() => {
-    // document.addEventListener('keydown', detectKeyDown, true);
-  }, [])
-
-  // const detectKeyDown = (e: any) => {
-  //   document.addEventListener('keydown')
-  //   console.log(e.key)
-  // }
-  
-  
-  
-  function chat_window() {
-    return (
-      <div className='chat-window-parent'>
-        Hello World
-        <div className='chat-input-bar'>
-          <div className='chat-input-div'>
-            <input type='text' placeholder='send a message' className='chat-input' />
-          </div>
-          {/* <button className='chat-btn' onClick={}>Send</button> */}
-        </div>
-      </div>
-    );
-  }
-
-  const toggleTheme = () => {
-    
-  }
-
-  function viewMode() {
-    return (
-      <div>
-        <button className='toggle-theme' onClick={toggleTheme}>ON|Off</button>
-      </div>
-    );
-  }
-  
+function App() {  
 
 
 
@@ -51,8 +10,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>{viewMode()}</div>
-      <div>{chat_window()}</div>
     </div>
   );
 }
