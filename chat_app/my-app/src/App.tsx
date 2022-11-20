2631ea1db20f9aeb134e9b187d3ed6b524bc788c
Nathan <nslee333@gmail.com>
2022-11-19 21:23:56 -0800

    [WIP]: Modified styling for chat button and input.

--- chat_app/my-app/src/App.tsx
+++ chat_app/my-app/src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react';
 import './App.css';
 
 function App() {
   const [messagesArray, setMessagesArray] = useState<string[]>([]);
+  const [draft, setDraft] = useState<string>('');
+  
+  useEffect(() => {
+    // document.addEventListener('keydown', detectKeyDown, true);
+  }, [])
 
-  const sendMessage = (event: any) => {
-    // const message: string = 'chat-input'.value;
-    console.log(event.target.value)
-    messagesArray.push(event.target.value);
+  const detectKeyDown = (e: any) => {
+    document.addEventListener('keydown')
+    console.log(e.key)
   }
   
   
@@ -19,9 +23,9 @@ function App() {
         Hello World
         <div className='chat-input-bar'>
           <div className='chat-input-div'>
-            <input type='text' placeholder='send a message' className='chat-input' form='chat' onChange={sendMessage}/>
+            <input type='text' placeholder='send a message' className='chat-input'  onChange={detectKeyDown}/>
           </div>
-          <button className='chat-btn' form='chat' onClick={sendMessage}>Button</button>
+          {/* <button className='chat-btn' onClick={}>Send</button> */}
         </div>
       </div>
     );
