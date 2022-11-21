3ae10f3b5e7ba3c2f9b77aaaae0af4742b628aa9
Nathan <nslee333@gmail.com>
2022-11-20 18:15:16 -0800

    [WIP]: Send button working.

--- chat_app/my-app/src/App.tsx
+++ chat_app/my-app/src/App.tsx
@@ -1,8 +1,36 @@
 import React, { useEffect } from 'react';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import './App.css';
 
 function App() {  
+  const [draft, setDraft] = useState("");
+
+  const ref = useRef('messageInput');
+  console.log(draft, "'Draft'");
+
+  useEffect(() => {
+    const keyDownHandler = (event: any) => {
+      if (event.key === 'Enter') {
+        // Might need to add a [&& inputValue !== ""] to make sure it only submits a text chat. 
+        setDraft(event.target.value);
+        
+
+      }
+    }
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    }
+
+  })
+
+  const handleSubmit = (event: any) => {
+    
+  }
+
+  
 
   function chat_window() {
     return (
@@ -10,8 +38,13 @@ function App() {
         <div className='chat-messages-window'>
           // Display current messages
         </div>
-        <input type='text' placeholder='Enter a message' className='input-message' />
-        <button className='send-btn'>Send</button>
+        <input 
+          type='text' 
+          id='messageInput'
+          placeholder='Enter a message' 
+          className='input-message'
+          />
+          <button type='button' className='send-btn' onClick={handleSubmit}>Send</button>
       </div>
     );
   }
