2ec15e6e73d933551f06e2648274963386224ab0
Nathan <nslee333@gmail.com>
2022-12-03 15:09:14 -0800

    Chore: Filled in Update / Delete http methods.

--- goal_tracker/backend/controllers/goalController.ts
+++ goal_tracker/backend/controllers/goalController.ts
@@ -36,7 +36,15 @@ const setGoal = asyncHandler(async (req: Request, res: Response) => {
 // @access Private
 
 const updateGoal = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400)
+        throw new Error("Goal not found.")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete Goals
@@ -44,7 +52,16 @@ const updateGoal = asyncHandler(async (req: Request, res: Response) => {
 // @access Private
 
 const deleteGoal = asyncHandler(async (req: Request, res: Response) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal) {
+        res.status(400)
+        throw new Error("Goal not found.")
+    }
+
+    await goal.remove();
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
