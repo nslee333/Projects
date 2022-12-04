3115fe595437eadeae96bfdefff9fea5688fb1be
Nathan <nslee333@gmail.com>
2022-12-03 17:36:45 -0800

    File chore: Deleted directories not in use.

--- testing_folder/test.js
+++
@@ -1,9 +0,0 @@
-var array = ['1', '.', '5', '3', '2', '1'];
-var stringNumber = '';
-for (var i = 0; i < array.length; i++) {
-    stringNumber += array[i];
-}
-var number = parseInt(stringNumber, (array.length + 1));
-console.log(array);
-console.log(stringNumber, "stringNumber");
-console.log(number, "number");
